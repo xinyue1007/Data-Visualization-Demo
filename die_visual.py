a3e685bc36343ea8d1f0e2db7e437a6b466e0a1e
xinyue1007 <851910295@qq.com>
2021-05-13 16:10:59 +0800

    add play die game

---
+++ die_visual.py
@@ -0,0 +1,33 @@
+import pygal as pygal
+
+from die import Die
+#创建一个D6
+die_1 = Die()
+die_2 = Die()
+#掷几次数字，并将结果存储在一个列表中
+results = []
+for roll_num in range(1000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
+
+print(results)
+
+#分析结果
+frequencies = []
+max_result = die_1.num_sides+die_2.num_sides
+
+for value in range(1,max_result+1):
+   frequency = results.count(value)
+   frequencies.append(frequency)
+print(frequencies)
+
+#针对结果进行可视化
+hist = pygal.Bar()
+hist.title = "Results of rolling two D6 1000 times "
+hist.x_labels = ['1','2','3','4','5','6','7','8','9','10','11','12']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6 +D6',frequencies)
+
+hist.render_to_file("die_visual.svg")
